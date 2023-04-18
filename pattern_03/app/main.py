20da31778ef31e0c14fa6bfa8c43baf80f23fbcf
g-kawano <go.departure@gmail.com>
2023-04-19 00:08:27 +0900

    pattern3 から DynamoDB 関連を削除

--- pattern_03/app/main.py
+++ pattern_03/app/main.py
@@ -1,5 +1,4 @@
 import boto3
-from app.libs.dynamodb import DynamoDBTable
 from app.setting import settings
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -26,10 +25,4 @@ def get_item(id: str):
     """
     アイテムを取得する
     """
-    table = DynamoDBTable("fastrunner-table")
-    item = table.get_item({"id": id})
-
-    if not item:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-    return item
+    pass
