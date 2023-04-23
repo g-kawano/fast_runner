b51287448da9532d2cd8213d00d0a1e45c1ee2e8
g-kawano <go.departure@gmail.com>
2023-04-23 10:40:38 +0900

    pattern3 に mysql のコンテナイメージを追加

---
+++ pattern_03/scripts/initDb.sql
@@ -0,0 +1,11 @@
+create table items(
+    id int auto_increment,
+    col1 text,
+    col2 text,
+    primary key (id)
+) ;
+
+insert into items(col1, col2)values('aaa', '1111');
+insert into items(col1, col2)values('bbb', '2222');
+insert into items(col1, col2)values('ccc', '3333');
+insert into items(col1, col2)values('ddd', '4444');
